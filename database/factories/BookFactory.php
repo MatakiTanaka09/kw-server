885374eb988cd191d7de3c128a438282e5123a53
Mataki Tanaka <mataki.tanaka.works@gmail.com>
2019-07-04 23:58:22 +0900

    Bookモデル修正

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -6,7 +6,8 @@ use KW\Infrastructure\Eloquents\Book;
 $factory->define(Book::class, function (Faker $faker) {
     return [
         'status' => $faker->randomElement([0, 5, 9, 10]),
-        'price' => $faker->randomElement([1000, 1500, 1800, 2000, 2500]),
+        'price'  => $faker->randomElement([1000, 1500, 1800, 2000, 2500]),
+        'remark' => $faker->text(200)
     ];
 });
 
